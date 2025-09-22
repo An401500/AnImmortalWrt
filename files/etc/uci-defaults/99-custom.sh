97174575dc24de22322a3b1ef94582896d26842a
An401500 <67913195+An401500@users.noreply.github.com>
2025-09-22 19:28:31 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -59,14 +59,12 @@ case "$board_name" in
 esac
 
 # 3. 配置网络
-if [ "$count" -eq 1 ]; then
-    # 单网口设备，DHCP模式
-    uci set network.lan.proto='dhcp'
-    uci delete network.lan.ipaddr
-    uci delete network.lan.netmask
-    uci delete network.lan.gateway
-    uci delete network.lan.dns
-    uci commit network
+#uci set network.lan.proto='dhcp'
+    #uci delete network.lan.ipaddr
+    #uci delete network.lan.netmask
+    #uci delete network.lan.gateway
+    #uci delete network.lan.dns
+    #uci commit network
 elif [ "$count" -gt 1 ]; then
     # 多网口设备配置
     # 配置WAN
@@ -105,8 +103,8 @@ elif [ "$count" -gt 1 ]; then
         uci set network.lan.ipaddr=$CUSTOM_IP
         echo "custom router ip is $CUSTOM_IP" >> $LOGFILE
     else
-        uci set network.lan.ipaddr='192.168.100.1'
-        echo "default router ip is 192.168.100.1" >> $LOGFILE
+        uci set network.lan.ipaddr='192.168.110.2'
+        echo "default router ip is 192.168.110.2" >> $LOGFILE
     fi
 
     # PPPoE设置
